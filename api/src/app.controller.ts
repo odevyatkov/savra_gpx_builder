df9dc29a07e1baad0158a1717c760566cbe2e035
Stanislav Bidenko <steve.bidenko@gmail.com>
2024-08-04 20:05:34 +0300

    Implemented getByRadius() and getByRectangle() in AppController

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { RectangleType } from 'gpx-common';
 import { AppService } from './app.service';
-import { RectanglePoints, VersionInfo } from './helpers';
+import { CirclePoints, RectanglePoints, VersionInfo } from './helpers';
 
 @ApiTags('Root') @Controller('v1')
 export class AppController {
@@ -13,17 +14,24 @@ export class AppController {
   }
 
   @Get('getByRectangle')
-  @ApiQuery({name: 'cell', type: Number, required: false, description: 'Cell size in km', example: '0.5'})
+  @ApiQuery({name: 'cell', type: Number, required: false, description: 'Cell size in km (default is 1km)', example: '0.5'})
   @ApiQuery({name: 'fromPoint', type: String, required: true, description: 'lat,lng of the first point in the rectangle', example: '50.050986,29.796902'})
-  @ApiQuery({name: 'toPoint', type: String, required: true, description: 'lat,lng of the second point in the rectangle', example: '50.050986,29.796902'})
-  getByRectangle(@Req() {query}: {query: RectanglePoints}) {
-    console.log({handler: 'getByRectangle', query});
+  @ApiQuery({name: 'toPoint', type: String, required: true, description: 'lat,lng of the opposite point in the rectangle', example: '50.022948,29.895159'})
+  getByRectangle(@Req() {query}: {query: RectanglePoints}): string {
+    const cellSize = query.cell ? parseFloat(query.cell) : 1;
+    const rectangleExtent = this.appService.getRectangleExtent(query, cellSize);
 
-    return 'getByRectangle';
+    return this.appService.buildGpx(rectangleExtent, cellSize);
   }
 
   @Get('getByRadius')
-  getByRadius() {
-    return 'getByRadius';
+  @ApiQuery({name: 'cell', type: Number, required: false, description: 'Cell size in km (default is 1km)', example: '0.5'})
+  @ApiQuery({name: 'center', type: String, required: true, description: 'lat,lng of the central point', example: '50.050986,29.796902'})
+  @ApiQuery({name: 'radius', type: Number, required: true, description: 'Radius in km', example: '3'})
+  getByRadius(@Req() {query}: {query: CirclePoints}): string {
+    const cellSize = query.cell ? parseFloat(query.cell) : 1;
+    const rectangleExtent: RectangleType = this.appService.getCircleExtent(query);
+
+    return this.appService.buildGpx(rectangleExtent, cellSize);
   }
 }
